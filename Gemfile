486055ae2600fade739da518020f6f314a3286d3
Angel Espinoza <a.espinozaflores@hotmail.com>
2017-03-13 15:37:08 -0600

    AJAX

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'coffee-rails', '~> 4.2'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -49,3 +50,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -18,9 +18,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   test "should create line_item" do
     assert_difference('LineItem.count') do
     post line_items_url, params: { product_id: products(:ruby.id) }
-  end
-
-    follow_redirect!
+  end  
+   follow_redirect!
     assert_select 'h2', 'Your Pramatic Cart'
     assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
     
@@ -48,5 +47,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to line_items_url
+  end
+  
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+        xhr: true
+    end
+    assert_response :success
+     assert_select_jquery :html, '#cart' do 
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/ 
+    end
   end
 end
